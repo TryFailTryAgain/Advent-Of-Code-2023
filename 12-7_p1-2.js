71232d7a218ac7c432742b4dcdcbcc25e2dc2788
TryFailTryAgain <115508207+TryFailTryAgain@users.noreply.github.com>
2023-12-19 18:14:45 -0500

    Day 7 p2 v1.0
    The logic is a terrible mess that should be mappings, but it works.

--- 12-7_p1-2.js
+++ 12-7_p1-2.js
@@ -24,6 +24,7 @@ class CardHand {
 /* Main */
 let hands = loadHands(data);
 solveP1(hands);
+solveP2(hands);
 
 /* End Main */
 
@@ -42,15 +43,15 @@ function loadHands(data) {
     return hands;
 }
 
-function getHandType(handObj) {
-    // Creates a set so we can easily identify the different card types used in this hand
+function getHandType(handObj, jokerIsWild = false) {
+    // Creates a set to remove duplicates and identify only how many card types we have in each hand
     const cardSet = new Set(handObj.cards);
     let cardGroups = [];
+    // For each card type, create a group of cards that match that type
     cardSet.forEach(cardType => {
-        // Regex to match all instances of a the current card type from the set
         cardGroups.push(handObj.cards.match(new RegExp(cardType, 'g')));
     });
-
+    // Sets the handType based on the number of groups of cards + some special cases
     switch (cardGroups.length) {
         case 1:
             handObj.handType = 7; // 5 of a kind
@@ -78,17 +79,92 @@ function getHandType(handObj) {
         default:
             console.log('---Something went wrong---');
     }
-    return;
+    // If we are checking solutions for part 2, then the joker card is w wild card and we need to update the handtype to 
+    //   account for the improvements that will be provided by the joker
+    if (jokerIsWild && cardSet.has('J')) {
+        let jokerGroupId = 0;
+        cardGroups.forEach((group, index) => {
+            if (group.includes('J')) {
+                jokerGroupId = index;
+                return;
+            }
+        });
+        let largestGroup = cardGroups.reduce((maxGroup, currentGroup) => currentGroup.length > maxGroup.length ? currentGroup : maxGroup, '');
+        switch (handObj.handType) {
+            case 7: // Five of a kind. No improvement
+                break;
+            case 6: // Four of a kind, improve to five of a kind
+                handObj.handType = 7;
+                break;
+            case 5: // Full house, improve to 4 or 5 of a kind
+                if (cardGroups[jokerGroupId].length == 1) {
+                    handObj.handType = 6;
+                } else {
+                    handObj.handType = 7;
+                }
+                break;
+            case 4: // Three of a kind, improves to 4 or 5 of a kind
+                if (largestGroup.includes('J')) {
+                    handObj.handType = 6;
+                    break;
+                }
+                if (cardGroups[jokerGroupId].length == 1) {
+                    handObj.handType = 6;
+                } else {
+                    handObj.handType = 7;
+                }
+                break;
+            case 3: // Two pairs, improves to full house, 4 of a kind, or 5 of a kind
+                if (cardGroups[jokerGroupId].length == 1) {
+                    handObj.handType = 5;
+                } else if (cardGroups[jokerGroupId].length == 2) {
+                    handObj.handType = 6;
+                } else {
+                    handObj.handType = 7;
+                }
+                break;
+            case 2: // One pair, improves to 3 of a kind, full house, 4 of a kind, or 5 of a kind
+                if (largestGroup.includes('J')) {
+                    handObj.handType = 4;
+                    break;
+                }
+                if (cardGroups[jokerGroupId].length == 1) {
+                    handObj.handType = 4;
+                } else if (cardGroups[jokerGroupId].length == 2) {
+                    handObj.handType = 6;
+                } else if (cardGroups[jokerGroupId].length == 3) {
+                    handObj.handType = 7;
+                }
+                break;
+            case 1: // High card, improves to one pair, two pair, full house, 4 of a kind, or 5 of a kind
+                if (cardGroups[jokerGroupId].length == 1) {
+                    handObj.handType = 2;
+                } else if (cardGroups[jokerGroupId].length == 2) {
+                    handObj.handType = 3;
+                } else if (cardGroups[jokerGroupId].length == 3) {
+                    handObj.handType = 5;
+                } else if (cardGroups[jokerGroupId].length == 4) {
+                    handObj.handType = 6;
+                } else {
+                    handObj.handType = 7;
+                }
+                break;
+            default:
+                console.log('---Something went wrong---');
+        }
+    }
 }
 
-function rankHands(hands) {
+function rankHands(hands, cardValues) {
     let handsByType = [];
-    // Object to convert card values to comparable numbers
-    const cardValues = { 'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2 };
 
     // Sets each hands handType
     hands.forEach(hand => {
-        getHandType(hand);
+        if (cardValues['J'] === 1) {
+            getHandType(hand, true);
+        } else {
+            getHandType(hand);
+        }
     });
 
     // Sorts the hands by handType, highest to lowest
@@ -132,9 +208,21 @@ function calculateScore(rankedHands) {
     return score;
 }
 
-function solveP1(hands){
-    console.log('Total winnings for Part 1: ' + calculateScore(rankHands(hands)));
+function solveP1(hands) {
+    // Object to convert card values to comparable numbers
+    const cardValues = { 'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2 };
+    console.log('Total winnings for Part 1: ' + calculateScore(rankHands(hands, cardValues)));
     return;
+}
+
+function solveP2(hands) {
+    // Object to convert card values to comparable numbers
+    const cardValues = { 'A': 14, 'K': 13, 'Q': 12, 'J': 1, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2 };
+    let rankedHands = rankHands(hands, cardValues);
+    //console.log(rankedHands.reverse().slice(0, 100));
+    let score = calculateScore(rankedHands);
+    console.log('Total winnings for Part 2: ' + score);
+
 }
 
 
